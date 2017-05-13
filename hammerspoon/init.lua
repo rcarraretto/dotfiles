8bc5774955cdf58e33353636eb6c31f698a8f306
Roberto Carraretto <rc.carraretto@gmail.com>
2017-05-13 13:57:56 -0300

    hammerspoon: basic volume control

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,15 +1,18 @@
 -- https://github.com/lodestone/hyper-hacks/blob/master/hammerspoon/init.lua
 -- https://gist.github.com/prenagha/1c28f71cb4d52b3133a4bff1b3849c3e
+-- https://github.com/Linell/hammerspoon-config/blob/master/init.lua
+
+local hyper = {'cmd','alt','ctrl'}
 
 -- Global variable for Hyper Mode
 k = hs.hotkey.modal.new({}, 'F17')
 
 -- Hyper+key setup in Alfred
-hyperBindings = {'1', '2', '3', '6', 't'}
+hyperBindings = {'1', '2', '3', '6', 'k', 't', 'y', '[', ']'}
 
 for i,key in ipairs(hyperBindings) do
   k:bind({}, key, nil, function()
-    hs.eventtap.keyStroke({'cmd','alt','ctrl'}, key)
+    hs.eventtap.keyStroke(hyper, key)
   end)
 end
 
@@ -25,3 +28,20 @@ end
 
 -- Bind the Hyper key
 f18 = hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
+
+local changeVolume = function(delta)
+  hs.audiodevice.defaultOutputDevice():setVolume(hs.audiodevice.current().volume + delta)
+end
+
+local deltaVol = 5
+
+local incVolume = function()
+  changeVolume(deltaVol)
+end
+
+local decVolume = function()
+  changeVolume(-deltaVol)
+end
+
+hs.hotkey.bind(hyper, ']', incVolume)
+hs.hotkey.bind(hyper, '[', decVolume)
