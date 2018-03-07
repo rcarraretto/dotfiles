2dd1af99441f552f2e96a679d7cfcb7e25ec7d60
Roberto Carraretto <rc.carraretto@gmail.com>
2018-03-07 09:19:29 +0100

    hammerspoon for macbook

---
+++ hammerspoon-macbook/init.lua
@@ -0,0 +1,32 @@
+local fnutils = require("hs.fnutils")
+
+local hyper = {'cmd', 'alt', 'ctrl'}
+
+--- App hotkeys
+local apps = {
+  {key = "1", name = "iTerm"},
+  {key = "2", name = "Google Chrome"},
+  {key = "3", name = "SourceTree"},
+  {key = "4", name = "Slack"},
+}
+local bind_hotkey = function(app)
+  hs.hotkey.bind(hyper, app.key, function()
+    hs.application.launchOrFocus(app.name)
+  end)
+end
+fnutils.each(apps, bind_hotkey)
+
+-- Reload config when any lua file in config directory changes
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == '.lua' then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
+hs.alert.show('Hammerspoon config loaded')
