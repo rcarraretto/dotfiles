86a030c097616e79e4c84263a33dfa347b6467bd
Roberto Carraretto <rc.carraretto@gmail.com>
2018-05-26 12:50:03 +0200

    improve macmini and global setup
    do hyper key using the new way (new version of karabiner elements)
    extract some common hammerspoon configs to common init file

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,3 +1,60 @@
+local fnutils = require("hs.fnutils")
+local hyper = {'cmd', 'alt', 'ctrl'}
+
+-- Window management
+hs.grid.setGrid('12x12')
+hs.grid.MARGINX = 0
+hs.grid.MARGINY = 0
+hs.window.animationDuration = 0.0
+
+local window_hotkeys = {
+  {key = "m", cell = '0,0 12x12'},
+  {key = "right", cell = '6,0 6x12'},
+  {key = "left", cell = '0,0 6x12'},
+  {key = "up", cell = '0,0 12x6'},
+  {key = "down", cell = '0,6 12x6'},
+}
+local bind_window_hotkey = function(hotkey)
+  hs.hotkey.bind(hyper, hotkey.key, function ()
+    local win = hs.window.frontmostWindow()
+    hs.grid.set(win, hotkey.cell)
+  end)
+end
+fnutils.each(window_hotkeys, bind_window_hotkey)
+
+
+-- Toggle iTerm and Chrome
+hs.hotkey.bind(hyper, "t", function()
+  if hs.application.frontmostApplication():title() == "iTerm2" then
+    hs.application.launchOrFocus("Google Chrome")
+  else
+    hs.application.launchOrFocus("iTerm")
+  end
+end)
+
+
+-- Display sleep (hyper + l)
+hs.hotkey.bind(hyper, "l", function()
+  hs.execute('pmset displaysleepnow')
+end)
+
+
+-- Reload config when any lua file in config directory changes
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == '.lua' then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
+hs.alert.show('Hammerspoon config loaded')
+
+
 if string.match(hs.host.localizedName(), 'rc-') then
   require("init_macbook")
 else
--- hammerspoon/init_macmini.lua
+++ hammerspoon/init_macmini.lua
@@ -6,45 +6,8 @@ local fnutils = require("hs.fnutils")
 local hyper = {'cmd', 'alt', 'ctrl'}
 local shift_hyper = {'cmd', 'alt', 'ctrl', 'shift'}
 
--- Global variable for Hyper Mode
-k = hs.hotkey.modal.new({}, 'F17')
-
--- Hyper+key setup
--- Some keys are handled here,
--- other keys are handled by programs like Alfred and Spectacle
-hyperBindings = {
-  '1', '2', '3', '4',
-  'c', 'k', 'i', 'm', 't', 'y',
-  '[', ']',
-  'up', 'down', 'left', 'right'
-}
-
-for i,key in ipairs(hyperBindings) do
-  k:bind({}, key, nil, function()
-    hs.eventtap.keyStroke(hyper, key)
-  end)
-  k:bind({'shift'}, key, nil, function()
-    hs.eventtap.keyStroke(shift_hyper, key)
-  end)
-end
-
--- Enter Hyper Mode when F18 (Hyper/Capslock) is pressed
-pressedF18 = function()
-  k:enter()
-end
-
--- Leave Hyper Mode when F18 (Hyper/Capslock) is pressed
-releasedF18 = function()
-  k:exit()
-end
-
--- Bind the Hyper key
-hs.hotkey.bind({}, 'F18', pressedF18, releasedF18)
-
-
-
 --- App hotkeys
-local apps = {
+local app_hotkeys = {
   {key = "1", name = "iTerm"},
   {key = "2", name = "Google Chrome"},
   {key = "3", name = "SourceTree"},
@@ -55,7 +18,7 @@ local bind_hotkey = function(app)
     hs.application.launchOrFocus(app.name)
   end)
 end
-fnutils.each(apps, bind_hotkey)
+fnutils.each(app_hotkeys, bind_hotkey)
 
 
 
@@ -91,20 +54,3 @@ hs.hotkey.bind(hyper, ']', incVolume)
 hs.hotkey.bind(hyper, '[', decVolume)
 hs.hotkey.bind(shift_hyper, ']', incVolumeSoft)
 hs.hotkey.bind(shift_hyper, '[', decVolumeSoft)
-
-
-
--- Reload config when any lua file in config directory changes
-function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == '.lua' then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
-end
-hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', reloadConfig):start()
-hs.alert.show('Hammerspoon config loaded')
