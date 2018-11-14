11fb379533bb7f0f40b6e0473cf544d0a380636e
Roberto Carraretto <rc.carraretto@gmail.com>
2018-11-14 09:49:09 +0100

    update app mappings

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -8,9 +8,9 @@ local app_hotkeys = {
   {key = "1", app = "iTerm"},
   {key = "2", app = "Google Chrome"},
   {key = "3", app = "SourceTree"},
-  {key = "4", app = "Slack"},
-  {key = "5", app = "Calendar"},
-  {key = "F1", app = "Spotify"},
+  {key = "4", app = "Calendar"},
+  {key = "5", app = "Slack"},
+  {key = "6", app = "Spotify"},
   {key = "F2", app = "Postman"},
   {key = "F3", app = "Studio 3T"},
 }
