fb080425eb36044f79d979b15511d48dd255dfc0
Roberto Carraretto <rc.carraretto@gmail.com>
2023-04-21 18:34:58 +0200

    feat(node): Add sample test

---
+++ node/src/req/index.spec.ts
@@ -0,0 +1,10 @@
+import { it } from '../test-runner';
+import { strict as assert } from 'node:assert';
+
+it('works', async () => {
+  assert.equal(2 + 2, 4);
+});
+
+it('works again', async () => {
+  assert.equal(2 + 2, 4);
+});
